4b5fe231cc2bccfd7315fb915fc5ee6550f5809d
ADEBISI WASIU <105849469+Adeventur@users.noreply.github.com>
2023-11-24 10:30:56 +0100

    add jquery

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+
+    $("h1").click(function() {
+        alert("This is a heading.");
+      });
+      
+      $("p").click(function() {
+        alert("This is a paragraph.");
+      });
+      
+      $("img").click(function() {
+        alert("This is an image.");
+      });
+
+
+
+
+
+
+});    
+    
+    
+    
+    
+    
+    
+  
